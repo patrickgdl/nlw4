bc5be6a5d38a5ca02fffc351b1d58a763e7a8b1b
Patrick Lima <patrickgdlima@gmail.com>
2021-02-24 14:02:51 -0300

    feat: init profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/patrickgdl.png" alt="Patrick Lima" />
+
+      <div>
+        <strong>Patrick Lima</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
